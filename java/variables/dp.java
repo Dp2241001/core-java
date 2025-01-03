231a0ed042d026b84d52974123224e58aab5edcc
Dp <adp2241001@gmail.com>
2025-01-03 14:17:01 +0530

    variables in java

---
+++ java/variables/dp.java
@@ -0,0 +1,11 @@
+public class dp
+{
+    public static void main(String[] args) {
+
+int a = 10;
+int b = 20;
+int result = a*b;
+     System.out.println( "result is " + ":" + result);   
+    }
+    // varable is udsed as reference of a value. 
+}
